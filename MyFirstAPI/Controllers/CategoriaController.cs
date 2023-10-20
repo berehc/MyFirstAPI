7d679d35a72b3885a3ffe28c6623812d0f984cf4
Olga Berenice Hernández Cruz <obhernandezc@gbm.com.mx>
2023-10-19 18:15:03 -0600

    Add cotegorias and tareas controllers

---
+++ MyFirstAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Models;
+using MyFirstAPI.Services;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        ICategoriaService _categoriaService;
+
+        public CategoriaController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(_categoriaService.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Categoria categoria)
+        {
+            _categoriaService.Save(categoria);
+            return Ok();
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id,[FromBody] Categoria categoria)
+        {
+            _categoriaService.Update(id,categoria);
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id) 
+        {
+            _categoriaService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ MyFirstAPI/Controllers/TareaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Models;
+using MyFirstAPI.Services;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TareaController : ControllerBase
+    {
+        ITareasService _tareasService;
+
+        public TareaController(ITareasService tareasService)
+        {
+            _tareasService = tareasService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_tareasService.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Tarea tarea)
+        {
+            _tareasService.Save(tarea);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] Tarea tarea)
+        {
+            _tareasService.Update(id, tarea);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            _tareasService.Delete(id);
+            return Ok();
+        }
+    }
+
+
+}
